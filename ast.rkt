170df205b5137f0fc029d911e142ea9f34d4dd7f
bitmole <robert@idea-loop.com>
2024-01-18 21:26:35 -0700

    Implement basic evaluator.

--- ast.rkt
+++ ast.rkt
@@ -3,3 +3,10 @@
 (define-type Exp
   [num (n : Number)]
   [plus (left : Exp) (right : Exp)])
+
+; evaluator type:
+;(calc : (Exp -> Number))
+(define (calc e)
+  (type-case Exp e
+    [(num n) n]
+    [(plus l r) (+ (calc l) (calc r))]))
