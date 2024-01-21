cb1e4d39e6df008821c124f88924693c9ca17c64
bitmole <robert@idea-loop.com>
2024-01-20 22:35:10 -0700

    calc tests

--- ast.rkt
+++ ast.rkt
@@ -10,3 +10,18 @@
   (type-case Exp e
     [(num n) n]
     [(plus l r) (+ (calc l) (calc r))]))
+
+(print-only-errors #true)
+(test (calc (num 1)) 1)
+(test (calc (num 2.3)) 2.3)
+(test (calc (plus (num 1) (num 2)))
+      3)
+(test (calc (plus (plus (num 1) (num 2)) (num 3)))
+      6)
+(test (calc (plus (plus
+                   (plus (num 1) (num 2))
+                   (num 3))
+                  (num 4)))
+      10)
+(test (calc (plus (num 0.1) (num 0.2))) 0.3)
+(test (calc (plus (num 0.1) (num 0.2))) 1/3)
